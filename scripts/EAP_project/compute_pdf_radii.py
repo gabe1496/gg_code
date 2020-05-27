12384f1b87d6859ccdb1c71bbfe11e3955ab8d5a
Gabrielle Grenier <gabrielle.g.grenier@usherbrooke.ca>
2020-05-27 15:10:50 -0400

    Add odf option and peaks option

--- scripts/EAP_project/compute_pdf_radii.py
+++ scripts/EAP_project/compute_pdf_radii.py
@@ -3,7 +3,6 @@
 Script to compute EAP.
 """
 import argparse
-import logging
 
 import nibabel as nib
 import numpy as np
@@ -15,9 +14,10 @@ from dipy.core.ndindex import ndindex
 from dipy.io.gradients import read_bvals_bvecs
 from dipy.direction.peaks import (peak_directions,
                                   reshape_peaks_for_visualization)
+from dipy.reconst.shore import ShoreModel
 
-from scilpy.reconst.shore_ozarslan import ShoreOzarslanModel
 from scilpy.utils.bvec_bval_tools import check_b0_threshold
+# from scilpy.reconst.shore_ozarslan import ShoreOzarslanModel
 
 
 def _build_arg_parser():
@@ -27,8 +27,8 @@ def _build_arg_parser():
     p.add_argument('in_diffusion',
                    help='Path of the input diffusion volume.')
 
-    p.add_argument('mask',
-                   help='Path of the mask.')
+    # p.add_argument('mask',
+    #                help='Path of the mask.')
 
     p.add_argument('bvals',
                    help='Path of the bvals file, in FSL format.')
@@ -43,11 +43,14 @@ def _build_arg_parser():
                    metavar='int', default=8, type=int,
                    help='Radial order used for the SHORE fit. (Default: 8)')
 
-    p.add_argument('--regul_weighting', action='store', dest='regul_weighting',
-                   metavar='float', default=0.2, type=float,
-                   help='Laplacian weighting for the regularization. '
-                        '0.0 will make the generalized cross-validation ' +
-                        '(GCV) kick in. (Default: 0.2)')
+    p.add_argument('--zeta', metavar='int', default=700, type=int,
+                   help='Scale factor.')
+
+    p.add_argument('--lambdaN', metavar='float', default=1e-8, type=float,
+                   help='Scale factor.')
+
+    p.add_argument('--lambdaL', metavar='float', default=1e-8, type=float,
+                   help='Scale factor.')
 
     p.add_argument('--sphere', default='repulsion724',
                    help='Type of sphere for the pdf compute.')
@@ -57,6 +60,12 @@ def _build_arg_parser():
 
     p.add_argument('--peaks', metavar='file', default='',
                    help='Output filename for the extracted peaks.')
+
+    p.add_argument('--odf', metavar='file', default='',
+                   help='Output filename for the odf.')
+
+    p.add_argument('--odf_peaks', metavar='file', default='',
+                   help='Output filename for the odf peaks.')
 
     return p
 
@@ -73,9 +82,9 @@ def main():
     check_b0_threshold(args, bvals.min())
     gtab = gradient_table(bvals, bvecs, b0_threshold=bvals.min())
 
-    mask = nib.load(args.mask).get_data().astype(np.bool)
-    voxels_with_values_mask = data[:, :, :, 0] > 0
-    mask = voxels_with_values_mask * mask
+    # mask = nib.load(args.mask).get_data().astype(np.bool)
+    # voxels_with_values_mask = data[:, :, :, 0] > 0
+    # mask = voxels_with_values_mask * mask
 
     sphere_rone = get_sphere(args.sphere)
     vertices = sphere_rone.vertices
@@ -85,42 +94,47 @@ def main():
 
     vertices_new = np.vstack((x, y, z)).T
 
-    if args.regul_weighting <= 0:
-        logging.info('Now computing SHORE ODF of radial order {0}'
-                     .format(args.radial_order) +
-                     ' and Laplacian generalized cross-validation')
+    shore_model = ShoreModel(gtab, radial_order=args.radial_order,
+                             zeta=args.zeta, lambdaN=args.lambdaN,
+                             lambdaL=args.lambdaL)
 
-        shore_model = ShoreOzarslanModel(gtab, radial_order=args.radial_order,
-                                         laplacian_regularization=True,
-                                         laplacian_weighting='GCV')
-    else:
-        logging.info('Now computing SHORE ODF of radial order {0}'
-                     .format(args.radial_order) +
-                     ' and Laplacian regularization weight of {0}'
-                     .format(args.regul_weighting))
+    # shore_model = ShoreOzarslanModel(gtab, radial_order=8,
+    #                                  laplacian_regularization=True,
+    #                                  laplacian_weighting=0.2)
 
-        shore_model = ShoreOzarslanModel(gtab, radial_order=args.radial_order,
-                                         laplacian_regularization=True,
-                                         laplacian_weighting=args.regul_weighting)
-
-    data = data[65:68, 81:84, 32:35]
+    # data = data[65:68, 81:84, 32:35]
     shape = data.shape[:-1]
     npeaks = 5
     smfit = shore_model.fit(data)
+
     pdf = smfit.pdf(vertices_new)
 
-    peaks_dirs = np.zeros((shape + (npeaks, 3)))
-    for idx in ndindex(shape):
-        direction, pk, ind = peak_directions(pdf[idx], sphere_rone)
-        n = min(npeaks, pk.shape[0])
-        peaks_dirs[idx][:n] = direction[:n]
-
     if args.peaks:
+        peaks_dirs = np.zeros((shape + (npeaks, 3)))
+        for idx in ndindex(shape):
+            direction, pk, ind = peak_directions(pdf[idx], sphere_rone)
+            n = min(npeaks, pk.shape[0])
+            peaks_dirs[idx][:n] = direction[:n]
         nib.save(nib.Nifti1Image(
             reshape_peaks_for_visualization(peaks_dirs), affine),
             args.peaks)
 
     nib.save(nib.Nifti1Image(pdf, affine), args.out_filename)
+
+    if args.odf:
+        odf = smfit.odf(sphere_rone)
+
+        if args.peaks:
+            peaks_dirs = np.zeros((shape + (npeaks, 3)))
+            for idx in ndindex(shape):
+                direction, pk, ind = peak_directions(odf[idx], sphere_rone)
+                n = min(npeaks, pk.shape[0])
+                peaks_dirs[idx][:n] = direction[:n]
+            nib.save(nib.Nifti1Image(
+                reshape_peaks_for_visualization(peaks_dirs), affine),
+                args.peaks)
+
+        nib.save(nib.Nifti1Image(odf, affine), args.out_filename)
 
 
 if __name__ == "__main__":
