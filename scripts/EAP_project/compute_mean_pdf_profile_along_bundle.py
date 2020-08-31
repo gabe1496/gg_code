32830f9aa6de99b4787b2cbd7154f4cea46fedaa
Gabrielle Grenier <gabrielle.g.grenier@usherbrooke.ca>
2020-08-31 09:03:27 -0400

    Mutliprocess

--- scripts/EAP_project/compute_mean_pdf_profile_along_bundle.py
+++ scripts/EAP_project/compute_mean_pdf_profile_along_bundle.py
@@ -5,7 +5,8 @@ import argparse
 
 import nibabel as nib
 import numpy as np
-from tqdm import tqdm
+import multiprocessing
+import itertools
 
 from dipy.reconst.mapmri import MapmriModel
 from dipy.core.gradients import gradient_table
@@ -65,24 +66,28 @@ def _build_arg_parser():
     return p
 
 
-# def compute_pdf_parallel(args):
-#     mapmri_model = args[0]
-#     vox_idx = args[1]
-#     seg = args[2]
-#     norm_weight = args[3]
-#     nb_points = args[4]
+def pdf_profile_parallel(args):
+    mapmri_fit = args[0]
+    vox_idx = args[1]
+    seg = args[2]
+    norm_weight = args[3]
+    nb_points = args[4]
+    r_sample = args[5]
+    corner = args[6]
 
-#     vox_idx = tuple(vox_idx)
-#     data_vox = data[vox_idx]
-#     mapmri_fit = mapmri_model.fit(data_vox)
+    pdf_map = np.zeros((vox_idx.shape[0], nb_points))
 
-#     r, theta, phi = cart2sphere(seg[0], seg[1], seg[2])
-#     theta = np.repeat(theta, args.nb_points)
-#     phi = np.repeat(phi, args.nb_points)
-#     x, y, z = sphere2cart(r_sample, theta, phi)
-#     r_points = np.vstack((x, y, z)).T
+    for idx in range(len(vox_idx)):
+        r, theta, phi = cart2sphere(seg[idx, 0], seg[idx, 1], seg[idx, 2])
+        theta = np.repeat(theta, nb_points)
+        phi = np.repeat(phi, nb_points)
+        x, y, z = sphere2cart(r_sample, theta, phi)
+        r_points = np.vstack((x, y, z)).T
+        pdf = mapmri_fit.pdf(r_points)
+        vox = tuple(vox_idx[idx] - corner)
+        pdf_map[idx] = pdf[vox] * norm_weight[idx]
 
-#     pdf = mapmri_fit.pdf(r_points) * norm_weight
+    return vox_idx, pdf_map
 
 
 def main():
@@ -103,6 +108,14 @@ def main():
     sft.to_vox()
     sft.to_corner()
 
+    bounding_box = sft.compute_bounding_box()
+    corner = np.array([np.min(bounding_box[:, 0]),
+                       np.min(bounding_box[:, 1]),
+                       np.min(bounding_box[:, 2])])
+    data_seg = data[np.min(bounding_box[:, 0]):np.max(bounding_box[:, 0]),
+                    np.min(bounding_box[:, 1]):np.max(bounding_box[:, 1]),
+                    np.min(bounding_box[:, 2]):np.max(bounding_box[:, 2])]
+
     # Fit the model
     if args.lap_reg == 1:
         mapmri_model = MapmriModel(gtab, radial_order=args.radial_order,
@@ -117,7 +130,7 @@ def main():
                                    laplacian_regularization=False,
                                    positivity_constraint=args.pos_const)
 
-    mapmri_fit = mapmri_model.fit(data)
+    mapmri_fit = mapmri_model.fit(data_seg)
     r_sample = np.linspace(0.0, 0.025, args.nb_points)
     all_crossed_indices = grid_intersections(sft.streamlines)
 
@@ -129,43 +142,45 @@ def main():
     crossed_indices = all_crossed_indices[20]
     segments = crossed_indices[1:] - crossed_indices[:-1]
     seg_lengths = np.linalg.norm(segments, axis=1)
-    print('segments', segments.shape)
 
     # Remove points where the segment is zero.
     # This removes numpy warnings of division by zero.
     non_zero_lengths = np.nonzero(seg_lengths)[0]
     segments = segments[non_zero_lengths]
     seg_lengths = seg_lengths[non_zero_lengths]
-    print('segments non zero', segments.shape)
 
     # Those starting points are used for the segment vox_idx computations
     strl_start = crossed_indices[non_zero_lengths]
     vox_indices = (strl_start + (0.5 * segments)).astype(int)
-    print('vox indices', vox_indices.shape)
 
     normalization_weights = np.ones_like(seg_lengths)
     if args.length_weighting:
         normalization_weights = seg_lengths / np.linalg.norm(vol.header.get_zooms()[:3])
 
-    for vox_idx, seg, norm_weight in tqdm(zip(vox_indices,
-                                              segments,
-                                              normalization_weights),
-                                          total=vox_indices.shape[0]):
-        vox_idx = tuple(vox_idx)
+    nbr_processes = multiprocessing.cpu_count()
+    chunks_vox = np.array_split(vox_indices, nbr_processes)
+    chunks_seg = np.array_split(segments, nbr_processes)
+    chunks_norm = np.array_split(normalization_weights, nbr_processes)
 
-        r, theta, phi = cart2sphere(seg[0], seg[1], seg[2])
-        theta = np.repeat(theta, args.nb_points)
-        phi = np.repeat(phi, args.nb_points)
-        x, y, z = sphere2cart(r_sample, theta, phi)
-        r_points = np.vstack((x, y, z)).T
+    pool = multiprocessing.Pool(nbr_processes)
+    results = pool.map(pdf_profile_parallel,
+                       zip(itertools.repeat(mapmri_fit),
+                           chunks_vox,
+                           chunks_seg,
+                           chunks_norm,
+                           itertools.repeat(args.nb_points),
+                           itertools.repeat(r_sample),
+                           itertools.repeat(corner)))
+    pool.close()
+    pool.join()
+    print(len(chunks_vox))
+    print(results.shape)
 
-        pdf = mapmri_fit.pdf(r_points)
+    #     weight_map[vox_idx] += norm_weight
+    #     eap_map[vox_idx] += pdf[vox_idx] * norm_weight
 
-        weight_map[vox_idx] += norm_weight
-        eap_map[vox_idx] += pdf[vox_idx] * norm_weight
-
-    nib.save(nib.Nifti1Image(weight_map, affine), args.weight_map)
-    nib.save(nib.Nifti1Image(eap_map, affine), args.eap_mean_map)
+    # nib.save(nib.Nifti1Image(weight_map, affine), args.weight_map)
+    # nib.save(nib.Nifti1Image(eap_map, affine), args.eap_mean_map)
 
 
 if __name__ == "__main__":
